e2e7cd6d11f5b9e22ca5bfb482b9bc3bad7c1316
cocooxy <1664535558@qq.com>
2019-11-30 17:29:01 +0800

    配置文件、引入element ui等

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="login">666
+        <el-button>默认按钮</el-button>
+    </div>
+    
+</template>
+
+
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f
+}
+</style>
+<script>
+export default {
+    name:'login'
+}
+</script>
